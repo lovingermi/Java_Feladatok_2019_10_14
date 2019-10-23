44b139375dc7d5440ab88d53fac0360abc30f107
Miklós Lővinger <lovingermi@gmail.com>
2019-10-23 14:38:43 +0200

    Feladat_2

--- 2019_10_19_11/src/Feladat_2019_10_19_11.java
+++ 2019_10_19_11/src/Feladat_2019_10_19_11.java
@@ -1,8 +1,12 @@
 
 public class Feladat_2019_10_19_11 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		Integer i= new Integer(Integer.MAX_VALUE);
+		System.out.println(i);
+		System.out.println(Integer.MIN_VALUE);
+		System.out.println("Byte min: "+Byte.MIN_VALUE+"Byte max: "+Byte.MAX_VALUE);
 
 	}
 
